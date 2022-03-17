5c9fab31475f8088896947f2e0f0d214c2855fdd
alejandro Gomez <alejandro.gomezg@autonoma.edu.co>
2022-03-17 17:32:55 -0500

    Se crea la tabla de egresos modelo y seeder alejandro

---
+++ database/migrations/2022_03_17_214337_create_egresos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEgresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('egresos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->enum('categoria',['Transporte','Alimentacion','Vivienda','Servicios','Cine','Fiesta','Otros'])->nullable()->default('Alimentacion');
+            $table->float('valor');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('egresos');
+    }
+}
