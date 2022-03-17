3277202d1e474de6d11038e488bb18d521988dff
Ricardo <richy.2603@gmail.com>
2022-03-16 23:37:40 -0500

    Traducción en las vistas de auth y layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Iniciaste sesión!') }}
                 </div>
             </div>
         </div>
