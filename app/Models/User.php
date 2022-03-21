cd5ea15c166608f2f615af0a1e98d8ceb1467712
Ricardo <richy.2603@gmail.com>
2022-03-21 14:53:49 -0500

    Se crean las funciones para ingresos y egresos

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Egreso;
+use App\Models\Ingreso;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function ingresos(){
+        return $this->hasMany(Ingreso::class);
+    }
+
+    public function egresos(){
+        return $this->hasMany(Egreso::class);
+    }
 }
