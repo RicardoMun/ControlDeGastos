63af5c319c816b084de6af942718d00269cc97c6
Ricardo <richy.2603@gmail.com>
2022-03-21 20:13:29 -0500

    Nuevo footer

--- resources/views/layouts/subview-footer.blade.php
+++ resources/views/layouts/subview-footer.blade.php
@@ -1,8 +1,5 @@
-
-<body class="d-flex flex-column">
-    <footer class="flex-shrink-0 py-4 text-black-50">
-      <div class="container text-center">
-        <p>Desarrollado en <strong>Programación Backend</strong> por <strong>Alejandro Gómez, Andrea Muñoz, Ricardo Muñoz, Cristian Santa</strong> © 2022 Copyright</p>
-      </div>
-    </footer>
-  </body>
+ <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Desarrollado en <strong>Programación Backend</strong> por <strong>Alejandro Gómez, Andrea Muñoz, Ricardo Muñoz, Cristian Santa</strong> © 2022 Copyright</small>
+    </div>
+</footer>
